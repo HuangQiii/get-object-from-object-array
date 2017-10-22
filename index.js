cccc9f8b0303a4187b4afb59bfb344ac1ffbaa75
祁煌 <huang.qi@hand-china.com>
2017-10-22 21:37:04 +0800

    兼容

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 'use strict';
 
 var getFirstIndex = function (arr, propertyName, value) {
-
+    if (typeof Array.prototype.findIndex != "function") {
+        for (var i = 0, length = arr.length; i < length; i++) {
+            if (arr[i][propertyName] == value)
+                return i;
+        }
+        return -1;
+    }
     return arr.findIndex(x => x[propertyName] == value);
 }
 
 var getFirst = function (arr, propertyName, value) {
+    if (typeof Array.prototype.find != "function") {
+        for (var i = 0, length = arr.length; i < length; i++) {
+            if (arr[i][propertyName] == value)
+                return arr[i];
+        }
+        return undefined;
+    }
     return arr.find(x => x[propertyName] == value);
 }
 
 var getAll = function (arr, propertyName, value) {
     if (typeof Array.prototype.filter != "function") {
-        Array.prototype.filter = function (fn, context) {
-            var array = [];
-            if (typeof fn === "function") {
-                for (var k = 0, length = this.length; k < length; k++) {
-                    fn.call(context, this[k], k, this) && array.push(this[k]);
-                }
-            }
-            return array;
-        };
-    }
+        var array = [];
+        for (var i = 0, length = arr.length; i < length; i++) {
+            if (arr[i][propertyName] == value)
+                array.push(arr[i]);
+        }
+        return array;
+    };
     return arr.filter(x => x[propertyName] == value);
 }
 
